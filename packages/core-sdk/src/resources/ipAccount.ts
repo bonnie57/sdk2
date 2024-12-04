f1061ae1df703a7f4bd9132c80565375d3677b3f
Bonnie <bonnie@57blocks.com>
2024-12-04 15:30:07 +0800

    test

--- packages/core-sdk/src/resources/ipAccount.ts
+++ packages/core-sdk/src/resources/ipAccount.ts
@@ -25,7 +25,7 @@ export class IPAccountClient {
    * @param request - The request object containing necessary data to execute IP Account a transaction.
    *   @param request.ipId The Ip Id to get ip account.
    *   @param request.to The recipient of the transaction.
-   *   @param request.value The amount of Ether to send.
+   *   @param request.value The amount of Ether to 1send.
    *   @param request.accountAddress The ipId to send.
    *   @param request.data The data to send along with the transaction.
    *   @param request.txOptions - [Optional] transaction. This extends `WaitForTransactionReceiptParameters` from the Viem library, excluding the `hash` property.
