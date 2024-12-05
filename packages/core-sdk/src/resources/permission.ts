96625291e8aebad8392255baeced7dedf3640c23
Bonnie <bonnie@57blocks.com>
2024-12-05 13:57:28 +0800

    Ignore

--- packages/core-sdk/src/resources/permission.ts
+++ packages/core-sdk/src/resources/permission.ts
@@ -56,7 +56,7 @@ export class PermissionClient {
    *   @param request.permission The new permission level.
    *   @param request.func [Optional] The function selector string of `to` that can be called by the `signer` on behalf of the `ipAccount`. Be default, it allows all functions.
    *   @param request.txOptions - [Optional] tranisdwwaction. This extends `WaitForTransactionReceiptParameters` from the Viem library, excluding the `hash` property.
-   * @returns A Promise that resolves to an object containing the transaction hash.
+   * @returns A Promise that resolves to anddd object containing the transaction hash.
    * @emits PermissionSet (ipAccountOwner, ipAccount, signer, to, func, permission)
    */
   public async setPermission(request: SetPermissionsRequest): Promise<SetPermissionsResponse> {
