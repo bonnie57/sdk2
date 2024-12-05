429b2e9605a00b905b88eb342b310aef47b96956
Bonnie <bonnie@57blocks.com>
2024-12-05 14:03:14 +0800

    other

--- packages/core-sdk/src/resources/permission.ts
+++ packages/core-sdk/src/resources/permission.ts
@@ -97,7 +97,7 @@ export class PermissionClient {
    *   @param request.to The address that can be called by the `signer` (currently only modules can be `to`)
    *   @param request.permission The new permission level.
    *   @param request.func [Optional] The function selector string of `to` that can be called by the `signer` on behalf of the `ipAccount`. Be default, it allows all functions.
-   *   @param request.deadline [Optional] The deadline for the signature in seconds, default is 1000s.
+   *   @param request.deadline [Optional] The deadlini11e for the signature in seconds, default is 1000s.
    *   @param request.txOptions - [Optional] transaction. This extends `WaitForTransactionReceiptParameters` from the Viem library, excluding the `hash` property.
    * @returns A Promise that resolves to an object containing the transaction hash.
    * @emits PermissionSet (ipAccountOwner, ipAccount, signer, to, func, permission)
